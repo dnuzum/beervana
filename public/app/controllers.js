0f2a36ba320cac2e45e762d231f172a62491f799
JonathanAndrew <jonandrewstevenson@yahoo.com>
2016-04-06 13:03:20 -0700

    font page styling

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -36,11 +36,32 @@ app.controller('SearchCtrl', ['$scope', '$http','$rootScope', '$document', funct
   };
 }]);
 
-app.controller("HomeCtrl", ["scope", function($scope){
-  $scope.animateElementIn = function($el) {
-    $el.removeClass('hidden');
-    $el.addClass('animated fadeInUp'); // this example leverages animate.css classes
+app.controller("HomeCtrl", ["$scope", function($scope){
+  $scope.elementIn = function($el) {
+    $el.css('visibility', 'visible');
+    $el.addClass('animated fadeIn'); // this example leverages animate.css classes
   };
+   $scope.elementOut = function($el) {
+    $el.css('visibility', 'hidden');
+    $el.removeClass('animated fadeIn'); // this example leverages animate.css classes
+  };
+  $scope.elementIn2 = function($el) {
+    $el.css('visibility', 'visible');
+    $el.addClass('animated fadeInDownBig'); // this example leverages animate.css classes
+  };
+   $scope.elementOut2 = function($el) {
+    $el.css('visibility', 'hidden');
+    $el.removeClass('animated fadeInDownBig'); // this example leverages animate.css classes
+  };
+  $scope.animateElementIn = function($el) {
+    $el.css('visibility', 'visible');
+    $el.addClass('animated fadeInDownBig'); // this example leverages animate.css classes
+  };
+  $scope.animateElementOut = function($el) {
+    $el.css('visibility', 'hidden');
+    $el.removeClass('animated fadeInDownBig'); // this example leverages animate.css classes
+  };
+
 }])
 
 
